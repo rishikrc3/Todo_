c51b1d96e115c5915e3d2b1cf4b1e176eb6577ab
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-18 08:44:34 +0530

    add tasks

--- todo/src/Form.js
+++ todo/src/Form.js
@@ -1,21 +1,27 @@
 import { useState } from "react";
+let nextId = 0;
 export default function Form() {
   const [message, setmessage] = useState("");
-  const [updated, setupdated] = useState(message);
+  const [tasks, settasks] = useState([]);
+  const [completedtasks, setcompletedtasks] = useState([]);
 
-  function handleChange(event) {
-    setmessage(event.target.value);
-  }
   function handleClick() {
-    setupdated(message);
+    settasks([...tasks, { id: nextId++, name: message }]);
   }
-
   return (
     <>
-      <input type="text" onChange={handleChange} value={message} />
+      <input onChange={(e) => setmessage(e.target.value)} value={message} />
       <button onClick={handleClick}>Enter</button>
 
-      <h2>{updated}</h2>
+      <h2>Completed tasks</h2>
+      <h2>Pending tasks</h2>
+      <ul>
+        {tasks.map((tasks) => (
+          <li key={tasks.id}>
+            {tasks.name} <button> Delete</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }

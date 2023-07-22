04e7ae81c7d0acd7eaa2541c9610f875cd24395d
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-22 23:28:17 +0530

    initial

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,32 +1,5 @@
-import Heading from "./Heading";
-import Form from "./Form";
-import { useState } from "react";
-import Tasks from "./Tasks";
-let nextId = 0;
-
 function App() {
-  const [todos, settodos] = useState([]);
-
-  // const addTodo = (todo) => {
-  //   settodos([...todos, { id: nextId++, name: todo }]);
-  //   console.log(todos);
-  // };
-  function addTodo(todo) {
-    settodos([...todos, { id: nextId++, name: todo }]);
-  }
-
-  function deleteTodo(id) {
-    settodos(todos.filter((todo) => todo.id !== id));
-  }
-  return (
-    <>
-      <Heading />
-      <Form addTodo={addTodo} />
-      {todos.map((name, index) => (
-        <Tasks tasks={name} index={index} deleteTodo={deleteTodo} />
-      ))}
-    </>
-  );
+  return <>h1</>;
 }
 
 export default App;
--- todo/src/Heading.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function Heading() {
-  return <h1>Todo List'</h1>;
-}

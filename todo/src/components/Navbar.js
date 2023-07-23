e141a5b3ba5129e02654aabd749a8e1a50825db3
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-23 21:51:10 +0530

    responsive_navbar

--- todo/src/components/Navbar.js
+++ todo/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -6,10 +6,15 @@ import {
   Button,
   IconButton,
   makeStyles,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
 } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 import InfoIcon from "@material-ui/icons/Info";
 import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom"; // If you are using React Router
 
 const useStyles = makeStyles((theme) => ({
@@ -33,6 +38,9 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     textDecoration: "none",
     color: "inherit",
+    [theme.breakpoints.down("sm")]: {
+      display: "none", // Hide the links on small screens
+    },
   },
   greenButton: {
     backgroundColor: "#426b1f", // Green background color
@@ -41,36 +49,81 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       backgroundColor: "#009900", // Darker green color on hover
     },
+    [theme.breakpoints.down("sm")]: {
+      display: "none", // Hide the login/signup button on small screens
+    },
+  },
+  menuIcon: {
+    marginLeft: "auto",
+    [theme.breakpoints.up("md")]: {
+      display: "none", // Hide the menu icon on medium and large screens
+    },
+  },
+  drawerList: {
+    width: 200,
   },
 }));
 
 const Navbar = () => {
   const classes = useStyles();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navLinks = (
+    <div>
+      {/* Replace '/' with the respective URLs for each link */}
+      <Button component={Link} to="/" className={classes.linkButton}>
+        <HomeIcon />
+      </Button>
+      <Button component={Link} to="/about" className={classes.linkButton}>
+        <InfoIcon />
+      </Button>
+      <Button component={Link} to="/contact" className={classes.linkButton}>
+        <MailIcon />
+      </Button>
+      {/* Green color button */}
+      <Button variant="contained" className={classes.greenButton}>
+        Login/Signup
+      </Button>
+    </div>
+  );
 
   return (
     <AppBar position="static" className={classes.appBar}>
       <Toolbar>
-        {/* Replace '/' with the homepage URL if using React Router */}
         <Link to="/" className={classes.title}>
           <Typography variant="h6">Code Buddy</Typography>
         </Link>
-        <div style={{ marginLeft: "auto" }}>
-          {/* Replace the '/' with the respective URLs for each link */}
-          <Button component={Link} to="/" className={classes.linkButton}>
-            <HomeIcon />
-          </Button>
-          <Button component={Link} to="/about" className={classes.linkButton}>
-            <InfoIcon />
-          </Button>
-          <Button component={Link} to="/contact" className={classes.linkButton}>
-            <MailIcon />
-          </Button>
-          {/* Green color button */}
-          <Button variant="contained" className={classes.greenButton}>
-            Login/Signup
-          </Button>
-        </div>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuIcon}
+          onClick={toggleMenu}
+        >
+          <MenuIcon />
+        </IconButton>
+        {navLinks}
       </Toolbar>
+      {/* Collapsible navigation menu for small screens */}
+      <Drawer anchor="right" open={isMenuOpen} onClose={toggleMenu}>
+        <div className={classes.drawerList} onClick={toggleMenu}>
+          <List>
+            <ListItem button component={Link} to="/">
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button component={Link} to="/about">
+              <ListItemText primary="About" />
+            </ListItem>
+            <ListItem button component={Link} to="/contact">
+              <ListItemText primary="Contact" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
     </AppBar>
   );
 };

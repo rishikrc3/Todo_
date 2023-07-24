1d2ad7c191ee8f4262d03b6fef642849319a13be
Rishik Chakravarty <rishikrc3@gmail.com>
2023-07-24 18:01:40 +0530

    routes

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Landing from "./components/Landing/Landing";
 import Login from "./components/Authentication_/Login";
 import Signup from "./components/Authentication_/Signup";
 
-import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {" "}
-      <Signup />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Add more routes here if needed */}
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
